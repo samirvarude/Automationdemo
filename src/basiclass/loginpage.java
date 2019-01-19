a5b467e0254153a32fb6b6957dffef9fff37f581
samirvarude <samir.varude@gmail.com>
2019-01-19 08:15:17 +0000

    samir weekend work

--- src/basiclass/loginpage.java
+++ src/basiclass/loginpage.java
@@ -5,6 +5,9 @@ public class loginpage {
 	public void loginpage()
 	{
 		System.out.println("Loginpage");
+		System.out.println("Google testing");
+		System.out.println("Google testing");
+		System.out.println("Google testing");
 		
 	}
 
